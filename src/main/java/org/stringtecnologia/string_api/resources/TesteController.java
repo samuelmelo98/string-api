23939478c092e9ec017fcf99576178c2fa2cf8a1
Your Name <you@example.com>
2026-03-14 19:01:37 -0300

    Cors ajust

--- src/main/java/org/stringtecnologia/string_api/resources/TesteController.java
+++ src/main/java/org/stringtecnologia/string_api/resources/TesteController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class TesteController {
 
     private final List<UserDTO> users = new CopyOnWriteArrayList<>();
