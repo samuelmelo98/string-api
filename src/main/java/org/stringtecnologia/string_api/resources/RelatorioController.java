b1a30625d4c4ae5f37b0e17cb1d52086ce94a7be
Samuel Melo <samuelmelo98@hotmail.com>
2026-04-03 22:00:37 -0300

    Capturando IP do cliente para auditória em ralatório

--- src/main/java/org/stringtecnologia/string_api/resources/RelatorioController.java
+++ src/main/java/org/stringtecnologia/string_api/resources/RelatorioController.java
@@ -65,13 +65,16 @@ public ResponseEntity<byte[]> gerarPdf(HttpServletRequest request) throws Except
 
     Map<String, Object> dados = new HashMap<>();
 
+    String ip = getClientIp(request);
+
+
     dados.put("usuario", "Samuel Silva");
     dados.put("data", "03/04/2026");
     dados.put("dataHora", "03/04/2026 15:00");
     dados.put("projeto", "Sistema RH");
     dados.put("resumo", "Relatório gerado com Spring Boot + Thymeleaf");
     dados.put("titulo", "Relatório de Teste");
-    dados.put("ip", "127.0.0.1");
+    dados.put("ip", ip);
     dados.put("total", "R$ 300");
 
     // ✅ CORRETO
@@ -94,6 +97,10 @@ public ResponseEntity<byte[]> gerarPdf(HttpServletRequest request) throws Except
 
     byte[] pdf = pdfService.gerarPdf(dados);
 
+    System.out.println("X-Forwarded-For: " + request.getHeader("X-Forwarded-For"));
+System.out.println("X-Real-IP: " + request.getHeader("X-Real-IP"));
+System.out.println("RemoteAddr: " + request.getRemoteAddr());
+
     return ResponseEntity.ok()
             .header("Content-Disposition", "attachment; filename=relatorio.pdf")
             .contentType(MediaType.APPLICATION_PDF)
@@ -108,5 +115,26 @@ public ResponseEntity<byte[]> gerarPdf(HttpServletRequest request) throws Except
     MatrixToImageWriter.writeToStream(matrix, "PNG", baos);
 
     return Base64.getEncoder().encodeToString(baos.toByteArray());
+}
+
+private String getClientIp(HttpServletRequest request) {
+
+    String xForwardedFor = request.getHeader("X-Forwarded-For");
+
+    if (xForwardedFor != null && !xForwardedFor.isEmpty()) {
+        return xForwardedFor.split(",")[0];
+    }
+
+    String realIp = request.getHeader("X-Real-IP");
+
+    if (realIp != null && !realIp.isEmpty()) {
+        return realIp;
+    }
+
+    System.out.println("X-Forwarded-For: " + request.getHeader("X-Forwarded-For"));
+System.out.println("X-Real-IP: " + request.getHeader("X-Real-IP"));
+System.out.println("RemoteAddr: " + request.getRemoteAddr());
+
+    return request.getRemoteAddr();
 }
 }
